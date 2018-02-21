a2de6d9bde15f42b51c6a5059943adabf29031ca
Clif Jackson <code@clifjackson.net>
2018-02-20 20:55:40 -0500

    Resolve conflict

--- gitApplyStash.sh
+++ gitApplyStash.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "You must supply a stash index "
+if [ "$#" -ne 1 ]
+then
+    printf "You must supply a stash index!\n"
     exit 0
 fi
 
-echo "Are you sure you want to apply stash@{$1}? [Y/n] "
-while read -r -n 1 -s answer; do
-    if [[ $answer != "Y" ]]; then
-        echo "Done "
-        exit 0
-    else
-        git stash apply stash@{$1}
-        git stash list
+git stash show -p stash@{$1}
+
+printf "Are you sure you want to apply stash@{$1}? [Y/n]"
+while read -r -n 1 -s answer
+do
+    if [[ $answer == "Y" ]]
+    then
         break
+    else
+        printf "\nNothing Done\n"
+        exit 0
     fi
 done
+
+ROOT="${BASH_SOURCE%}"
+PARENTDIR=$(dirname $ROOT)
+
+printf "\n"
+
+git stash apply stash@{$1} && printf "\nStash $1 applied.\n" && source $PARENTDIR/gitDeleteStash.sh
 
 exit 0
