e5891c7ba64e37d708ef861c9619e95186454eb6
Clif Jackson <code@clifjackson.net>
2018-02-20 20:51:52 -0500

    Added files

---
+++ gitDeleteStash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+    printf "You must supply a stash index\n"
+    exit 0
+fi
+
+printf "Would you like to delete this stash now? [Y/n]:"
+while read -r -n 1 -s answer
+do
+    if [[ $answer == "Y" ]]
+    then
+        break
+    else
+        printf "\nNothing Done\n"
+        exit 0
+    fi
+done
+
+printf "\nAre you sure? [Y/n]:"
+while read -r -n 1 -s answer
+do
+    if [[ $answer == "Y" ]]
+    then
+        break
+    else
+        exit 0
+    fi
+done
+
+printf "\n"
+
+git stash drop stash@{$1} && git stash list
+
+exit 0
